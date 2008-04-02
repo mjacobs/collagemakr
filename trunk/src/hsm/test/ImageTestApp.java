d6a27b3ee33c5c4f39c15524fa32ca6815da4316
brianjmoore <brianjmoore@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-02 03:54:27 +0000

    - Added ImageTestApp, which is a simple image viewer for testing purposes. - Modified FlickrSource slightly to give better, faster results from getRandomImage. - Changed signature of ImageSource.getRandomImage.  Now returns null on error.

---
+++ trunk/src/hsm/test/ImageTestApp.java
@@ -0,0 +1,92 @@
+package hsm.test;
+
+import hsm.image.FlickrSource;
+import hsm.image.ImageException;
+import hsm.image.ImageSource;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class ImageTestApp {
+
+	private class ImageFrame extends JFrame
+	{
+		private static final long serialVersionUID = 1L;
+		BufferedImage _image;
+		
+		public ImageFrame()
+		{
+			setSize(640, 480);
+		}
+		
+		public void setImage(BufferedImage img)
+		{
+			_image = img;
+			
+			repaint();
+		}
+		
+		public void paint(Graphics g)
+		{
+			Graphics2D g2d = (Graphics2D)g;
+			
+			super.paint(g);
+			
+			if (_image != null)
+			{
+				g2d.drawImage(_image, 0, 0, this);
+			}
+		}
+	}
+	
+	private class ChangeImagePerformer implements ActionListener
+	{
+		ImageFrame _frame;
+		ImageSource _src;
+		
+		public ChangeImagePerformer(ImageFrame inFrame)
+		{
+			_frame = inFrame;
+			try {
+				_src = new FlickrSource();
+			} catch (ImageException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public void actionPerformed(ActionEvent evt) 
+		{
+    	 	_frame.setImage(_src.getRandomImage().getImage());
+		}
+		
+	}
+	
+	public ImageTestApp() throws ImageException, InterruptedException
+	{
+		ImageFrame frame = new ImageFrame();
+		
+		frame.setVisible(true);
+		
+		int delay = 1000; //milliseconds
+		ActionListener taskPerformer = new ChangeImagePerformer(frame);
+		new Timer(delay, taskPerformer).start();
+	}
+	
+	/**
+	 * @param args
+	 * @throws ImageException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws ImageException, InterruptedException {
+		
+		new ImageTestApp();
+	}
+
+}
