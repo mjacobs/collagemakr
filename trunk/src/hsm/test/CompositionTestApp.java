d0e174220a8b442fd07d9a6691332d5df55c2281
brianjmoore <brianjmoore@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-08 07:26:07 +0000

    Made CompositionTestApp demoable... maybe

--- trunk/src/hsm/test/CompositionTestApp.java
+++ trunk/src/hsm/test/CompositionTestApp.java
@@ -1,5 +1,6 @@
 package hsm.test;
 
+import hsm.image.BufferedImageOpAdaptor;
 import hsm.image.CompositeAdaptor;
 import hsm.image.Composition;
 import hsm.image.ElementNode;
@@ -8,12 +9,19 @@ import hsm.image.FlickrSource;
 import hsm.image.ImageException;
 import hsm.image.ImageSource;
 import hsm.image.OperationNode;
+import hsm.tools.Extraktor;
 
+import java.awt.AlphaComposite;
+import java.awt.Composite;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+
+import com.jhlabs.image.OpacityFilter;
 
 /**
  * @author bjmoore
@@ -51,6 +59,7 @@ public class CompositionTestApp {
 	}
 	
 	private ImageSource _source;
+	private Extraktor _extract;
 	
 	public CompositionTestApp() throws ImageException
 	{
@@ -63,24 +72,71 @@ public class CompositionTestApp {
 
 		System.out.println("initializing...");
 		_source = new FlickrSource();
+		_extract = new Extraktor();
 		frame.setVisible(true);
 		
-		compFrame.setComposition(new Composition(randomNode()));
-		
+		compFrame.setComposition(new Composition(randomNode(3)));
+		System.out.println("Complete");
 	}
 	
-	private ExpressionNode randomNode()
+	private ExpressionNode randomNode(int maxDepth)
 	{
-		if (Math.random() <= 0.5)
+		double r = Math.random();
+		if (r > 0.99 || maxDepth == 0)
 		{
 			System.out.println("creating element");
-			return new ElementNode(_source.getRandomImage().getImage());
+			return new ElementNode(_extract.getExtract(_source.getRandomImage().getImage()));
+		}
+		else if (r > 0.5)
+		{
+			Composite comp;
+			
+			double r2 = Math.random();
+			
+			if (r2 > 0.6)
+			{
+				comp = AlphaComposite.DstOver;
+			}
+			else
+			{
+				comp = AlphaComposite.Xor;
+			}
+			
+			System.out.println("creating composite operation");
+			// choose random operation
+			return new OperationNode(new CompositeAdaptor(comp), randomNode(maxDepth-1), randomNode(maxDepth-1));
+		}
+		else if (r > 0.2)
+		{
+			System.out.println("creating opacity operation");
+			// choose random operation
+			return new OperationNode(new BufferedImageOpAdaptor(new OpacityFilter((int)Math.round(Math.random()*0.8*256))), randomNode(maxDepth-1));
 		}
 		else
 		{
-			System.out.println("creating operation");
+			AffineTransform af;
+			double r2 = Math.random();
+			if (r2 > 1.0)
+			{
+				af = AffineTransform.getRotateInstance(Math.random()*2.0*Math.PI);
+				System.out.println("creating rotate operation");
+			}
+			else if (r2 > 0.5)
+			{
+				af = AffineTransform.getTranslateInstance(Math.random()*500, Math.random()*500);
+				System.out.println("creating translate operation");
+			}
+			else
+			{
+				System.out.println("creating scale operation");
+				af = AffineTransform.getScaleInstance(Math.random()*1.5 + 0.5, Math.random()*1.5 + 0.5);
+			}
+			
+			System.out.println(af.toString());
+			
+			
 			// choose random operation
-			return new OperationNode(new CompositeAdaptor(), randomNode(), randomNode());
+			return new OperationNode(new BufferedImageOpAdaptor(new AffineTransformOp(af, AffineTransformOp.TYPE_BILINEAR)), randomNode(maxDepth-1));
 		}
 	}
 	
