998b6f9bbcdadc6dc3a1fc8a800bc2d0fdf074e1
brianjmoore <brianjmoore@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-11 23:54:34 +0000

    Created Organism, Population classes

--- src/hsm/evo/ExpressionNode.java
+++ src/hsm/evo/ExpressionNode.java
@@ -6,9 +6,7 @@ public abstract class ExpressionNode {
 	public abstract LayerImage evaluate();
 	public abstract ExpressionNode createMutated();
 	public abstract ExpressionNode createMated(ExpressionNode friend);
+	
 	public abstract void print(String prefix);
-	public void print()
-	{
-		print("");
-	}
+	public void print() { print(""); }
 }
