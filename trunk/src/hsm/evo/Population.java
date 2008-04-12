2d8076f8c3f343653552704c7ff9e03fa5f46af5
jacobs.mh <jacobs.mh@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-12 00:45:07 +0000

    Added to Population and changed one function parameter name in Organism... teehee.

--- trunk/src/hsm/evo/Population.java
+++ trunk/src/hsm/evo/Population.java
@@ -1,28 +1,41 @@
 package hsm.evo;
 
-public class Population {
-	
+import java.util.ArrayList;
+
+public class Population
+{
+
+	protected int _generation;
+
+	protected Organism[] _orgs;
+
 	public Population(Organism[] orgs, int generation)
 	{
-		// TODO implement me! (assigned to mjacobs)
+		_orgs = orgs;
+		_generation = generation;
 	}
-	
+
 	public int getGeneration()
 	{
-		// TODO implement me! (assigned to mjacobs)
-		return 0;
+		return _generation;
 	}
-	
+
 	public Organism[] getOrganisms()
 	{
-		// TODO implement me! (assigned to mjacobs)
-		return null;
+		return _orgs;
 	}
-	
+
 	public static Population randomPopulation(int popSize)
 	{
-		// TODO implement me! (assigned to mjacobs)
-		return null;
+		Organism[] orgs = new Organism[] {};
+		ArrayList<Organism> arrayList = new ArrayList<Organism>();
+
+		for (int i = 0; i < popSize; i++)
+		{
+			arrayList.add(Organism.randomOrganism());
+		}
+
+		return new Population(arrayList.toArray(orgs), 1);
 	}
 
 	public Population nextGeneration()
