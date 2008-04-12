7d8c1599a39d79a8864797ae2fb3673f27a571e7
jacobs.mh <jacobs.mh@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-12 00:45:07 +0000

    Added to Population and changed one function parameter name in Organism... teehee.

--- src/hsm/evo/Organism.java
+++ src/hsm/evo/Organism.java
@@ -1,40 +1,44 @@
 package hsm.evo;
 
-public class Organism {
+public class Organism
+{
 	private Composition _composition;
+
 	private double _fitness;
-	
+
 	public Organism(Composition comp)
 	{
 		_composition = comp;
 		_fitness = 0.0;
 	}
-	
+
 	public Composition getComposition()
 	{
 		return _composition;
 	}
-	
+
 	public double getFitness()
 	{
 		return _fitness;
 	}
-	
+
 	public void setFitness(double fit)
 	{
 		_fitness = fit;
 	}
-	
+
 	public Organism mutatedCopy()
 	{
-		return new Organism(new Composition(_composition.getRoot().createMutated()));
+		return new Organism(new Composition(_composition.getRoot()
+				.createMutated()));
 	}
-	
-	public Organism matedCopy(Organism friend)
+
+	public Organism matedCopy(Organism ladyFriend)
 	{
-		return new Organism(new Composition(_composition.getRoot().createMated(friend.getComposition().getRoot())));
+		return new Organism(new Composition(_composition.getRoot().createMated(
+				ladyFriend.getComposition().getRoot())));
 	}
-	
+
 	public static Organism randomOrganism()
 	{
 		return new Organism(Composition.makeRandomComposition());
