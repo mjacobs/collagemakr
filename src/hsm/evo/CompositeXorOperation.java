cce51367c6c734e0955b27b66bdbf55b7dba3bc8
brianjmoore <brianjmoore@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-17 22:06:36 +0000

    - Messed with some operation probabilities - Switched to smaller images

--- src/hsm/evo/CompositeXorOperation.java
+++ src/hsm/evo/CompositeXorOperation.java
@@ -9,7 +9,7 @@ public class CompositeXorOperation extends ParametrizedOperationAdaptor {
 	static
 	{
 		OperationMetadata.getInstance().registerOperation(CompositeXorOperation.class);
-		OperationMetadata.getInstance().setOperationProbability(CompositeXorOperation.class, 5.0);
+		OperationMetadata.getInstance().setOperationProbability(CompositeXorOperation.class, 1.0);
 	}
 	
 	public CompositeXorOperation()
