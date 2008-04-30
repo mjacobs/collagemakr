0631be8da1f48aceb30a7c920555b174912992bc
jacobs.mh <jacobs.mh@334ddaec-d449-0410-be82-c182c8b986f0>
2008-04-30 22:37:52 +0000

    Stencil for TagChecker

---
+++ trunk/src/hsm/tools/TagChecker.java
@@ -0,0 +1,23 @@
+package hsm.tools;
+
+import java.util.List;
+
+import com.aetrion.flickr.tags.Tag;
+
+public class TagChecker
+{
+	public boolean isValidTag(Tag tag)
+	{
+		return false;
+	}
+	
+	public List<Tag> getTags()
+	{
+		return null;
+	}
+	
+	public List<Tag> getTags(int numTags)
+	{
+		return null;
+	}
+}
